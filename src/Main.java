4c7aef69ae70fcc605c16012682c48262f78e6b7
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-05-05 11:41:20 -0300

    Inexercicio

---
+++ src/Main.java
@@ -0,0 +1,98 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("=========================================================");
+        System.out.println("Exemplo do calculo do retangulo com funções");
+        System.out.println("=========================================================");
+
+        calcularSomaAreas();
+
+
+        System.out.println("=========================================================");
+        System.out.println("Exemplo de mostrar lista de números com funções");
+        System.out.println("=========================================================");
+        mostrarTodosOsValoresListaNumero(retornarListaNumeros());
+
+
+        System.out.println("=========================================================");
+        List<Integer> listaNumerosArray = retornarListaNumeros();
+        mostrarTodosOsValoresListaNumero(listaNumerosArray);
+
+
+        System.out.println("=========================================================");
+        System.out.println("Exemplo de mostrar lista de quantidade de nomes com funções");
+        System.out.println("=========================================================");
+        List<String> listaNomesComQuantidade = mostrarNomesComPosicao(retornarListaNumeros());
+        System.out.println("Exemplo de array = " + listaNomesComQuantidade);
+
+
+    }
+
+    public static double calcularAreaRetanguloComInputUsuario() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a base: ");
+        double base = scanner.nextDouble();
+
+        System.out.println("Digite a altura: ");
+        double altura = scanner.nextDouble();
+
+        double areaRetangulo = base * altura;
+        System.out.println("O valor da área é de " + areaRetangulo);
+
+        return areaRetangulo;
+    }
+
+    public static void calcularAreaRetangulo(double base, double altura) {
+        double areaRetangulo = base * altura;
+        System.out.println("O valor da área é de " + areaRetangulo);
+    }
+
+    public static void calcularSomaAreas() {
+        //Exemplo de uso do retorno de outra função
+        double somaAreas = calcularAreaRetanguloComInputUsuario() + calcularAreaRetanguloComInputUsuario();
+        System.out.println("A somatoria da area total é de: " + somaAreas);
+    }
+
+    public static List<Integer> retornarListaNumeros() {
+        List<Integer> listaNumero = new ArrayList<>(3);
+        listaNumero.add(1);
+        listaNumero.add(2);
+        listaNumero.add(3);
+        return listaNumero;
+    }
+
+    public static void mostrarTodosOsValoresListaNumero(List<Integer> numeros) {
+        for (int i = 0; i < numeros.size(); i++) {
+            System.out.println(numeros.get(i));
+        }
+    }
+
+    public static List<String> mostrarNomesComPosicao(List<Integer> quantidade) {
+        List<String> nomesExibir = new ArrayList<>();
+        for (int i = 0; i < quantidade.size(); i++) {
+            nomesExibir.add(quantidade.get(i) +" nomes");
+            System.out.println(nomesExibir.get(i));
+        }
+        return nomesExibir;
+    }
+
+    public static List<String> retornarListaNomes() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Digite a capacidade: ");
+        int capacidade = scanner.nextInt();
+
+        List<String> listaNomes = new ArrayList<>();
+
+        for (int i = 0; i <=capacidade; i++) {
+            System.out.println("Digite um nome: ");
+            listaNomes.add(scanner.nextLine());
+        }
+        return listaNomes;
+    }
+}
